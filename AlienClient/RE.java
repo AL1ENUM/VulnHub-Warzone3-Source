0486bb4d71af927fb468db004143be5936dc171b
alienum <67610662+AL1ENUM@users.noreply.github.com>
2020-12-14 21:03:27 +0200

    Create RE.java

---
+++ AlienClient/RE.java
@@ -0,0 +1,75 @@
+package alien;
+
+import java.io.Serializable;
+
+public class RE implements Serializable {
+	
+	private Token token;
+	private String option;
+	private String cmd;
+	private String value;
+	
+	public RE() {
+		
+	}
+	
+	
+	public RE(Token token, String option, String cmd, String value) {
+		
+		this.token = token;
+		this.option = option;
+		this.cmd = cmd;
+		this.value = value;
+	}
+
+
+	public Token getToken() {
+		return token;
+	}
+
+
+	public void setToken(Token token) {
+		this.token = token;
+	}
+
+
+	public String getOption() {
+		return option;
+	}
+
+
+	public void setOption(String option) {
+		this.option = option;
+	}
+
+
+	public String getCmd() {
+		return cmd;
+	}
+
+
+	public void setCmd(String cmd) {
+		this.cmd = cmd;
+	}
+
+
+	public String getValue() {
+		return value;
+	}
+
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+
+	@Override
+	public String toString() {
+		return "RE [ " + token + ", option=" + option + ", cmd=" + cmd + ", value=" + value + "]";
+	}
+	
+	
+	
+	
+
+}
